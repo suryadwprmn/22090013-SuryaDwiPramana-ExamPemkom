2baa2c5b8ce68d213cb80c38e928f0e6b0f2fc80
suryadwprmn <suryadwipramana18@gmail.com>
2023-11-05 15:23:51 +0700

    Process Desain UI

---
+++ src/com/pointofsales/utils/ConnectionDb.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pointofsales.utils;
+
+import com.mysql.cj.xdevapi.Result;
+import com.mysql.cj.xdevapi.Statement;
+import com.sun.jdi.connect.spi.Connection;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class ConnectionDb {
+    private static Connection con;
+    private static Statement St;
+    private String sql ="";
+    p
+    public static void main(String args[]){
+
+    }
+}
